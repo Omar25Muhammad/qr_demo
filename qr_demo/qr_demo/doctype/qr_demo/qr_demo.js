efc734f2c594fa32018893237b503aea5ead44bd
Omar M Shehada <o.shehada@ard.ly>
2023-05-02 18:59:39 +0200

    Fixes

--- qr_demo/qr_demo/doctype/qr_demo/qr_demo.js
+++ qr_demo/qr_demo/doctype/qr_demo/qr_demo.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 let barcode = '';
-
+let original_barcode = '';
 
 frappe.ui.form.on('QR Demo', {
 	refresh: function (frm) {
@@ -23,25 +23,10 @@ frappe.ui.form.on('QR Demo', {
 					// frm.doc.item_barcode = r.message
 					// frm.refresh_fields('item_barcode')
 					if (r.message != -1)
-						{
-						// barcode = r.message.replace(r.message[0], '9')
-						
-						frm.call({
-							method: 'qr_demo.qr_code.scan_barcode',
-							args: { doc: frm.doc.item_code },
-							callback: function (ft) {
-								console.log(ft.message)
-							frm.call({
-								method: 'qr_demo.qr_code.add_barcode',
-								args: { barcode: ft.message, docname: frm.doc.item_code },
-								callback: function (f) {
-									console.log(f.message)
-								}
-							});
-
-								
-							}
-						});
+					{
+						original_barcode = r.message
+						console.log('The original: ' + original_barcode)
+						barcode = r.message.replace(r.message[0], '9')
 						frm.call({
 							method: "qr_demo.qr_code.get_barcode",
 							args: { data: barcode },
@@ -71,9 +56,10 @@ frappe.ui.form.on('QR Demo', {
 		if ((frm.doc.item_code) && (frm.doc.uom)) {
 				frm.call({
 					doc: frm.doc,
-					args: {price_list: frm.doc.price_list, item_barcode: barcode, uom: frm.doc.uom},
 					method: "the_price",
+					args: {price_list: frm.doc.price_list, item_barcode: original_barcode, uom: frm.doc.uom},
 					callback: function (response) { 
+						console.log(original_barcode)
 							console.log(response.message)
 							frm.doc.item_price = response.message
 							frm.refresh_fields()
@@ -87,5 +73,23 @@ frappe.ui.form.on('QR Demo', {
 			frappe.msgprint('الرجاء التأكّد من اختيار كل من الصنف ووحدة الكمية')
 			frm.refresh_fields()
 		}
-		}
+	},
+	after_save: function (frm) {
+		frm.call({
+							method: 'qr_demo.qr_code.scan_barcode',
+							args: { doc: frm.doc.name },
+							callback: function (ft) {
+								console.log(ft.message)
+							frm.call({
+								method: 'qr_demo.qr_code.add_barcode',
+								args: { barcode: ft.message, docname: frm.doc.item_code },
+								callback: function (f) {
+									console.log(f.message)
+								}
+							});
+
+								
+							}
+						});
+	}
 });
